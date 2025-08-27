d61029e96bc25fb3b3d0ad88c2331a45d8a206e3
lenguyenquangminh <lenguyenquangminh2554@gmail.com>
2025-08-27 20:46:16 +0700

    Fix lỗi Export CSV 2.0 - Sửa thứ tự route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('customers')->group(function () {
+    Route::get('export', [CustomerController::class, 'export'])->name('customers.export');
 });
-
 Route::resource('customers', CustomerController::class);
-Route::get('/customers/export', [CustomerController::class, 'export'])->name('customers.export');
